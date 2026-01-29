da1b247708f048c961d04a8f4dd9dfc426d74c42
vikr2629 <vikramdesmoines@gmail.com>
2026-01-29 16:01:41 +0530

    cors  issue resolved

--- src/app.js
+++ src/app.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const cors = require('cors');
 const urlshorter = require('./routes/urlshorter.routes.js')
 const app = express();
 
+app.use(cors({
+    origin:[
+        "http://localhost:5173",
+        "https://shorty.netlify.app"
+    ],
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"]
+}));
 app.use(express.json())
+
 
 //routes
 app.use('/api/shorturl', urlshorter)
